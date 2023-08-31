c71ca63308ad93feac39b62cae9e4038ffa0a188
SepehrAPlus <mtnaaa12864@gmail.com>
2023-08-31 17:00:56 +0430

    .

--- KianV0_App/urls.py
+++ KianV0_App/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('index/', views.view_of__index),
-    path('un1/', views.view_of__landing_views_counter_incremnet), #this one will return what nth visitor are your; cosmetic one
+    path("IntroLandingPage/", views.view__IntroLandingPage),
+    path("MainPage/", views.view__MainPage)
 ]
--- KianV0_App/views.py
+++ KianV0_App/views.py
@@ -2,15 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 
-# Create your views here.
-def view_of__index(request):
-	return HttpResponse("hello :)")
 
+def view__IntroLandingPage(request):
+	return ""
 
-def view_of__landing_views_counter_incremnet(request):
-	pass
-
-def view_of__search_shipitems_by(request):
-	"""
-	A post one 
-	"""
+def view__MainPage(request):
+	return ""
